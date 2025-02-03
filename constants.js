e19ba3cd32f5a68b459e85d829656b02d13acfc5
Amidou D. <72388952+Kazuma746@users.noreply.github.com>
2025-02-03 15:20:18 +0100

    Feat/playlist player (#30)
    * FIX: playlist privé s'affichent
    
    * la lecture sur les pages playlist est ok
    
    ---------
    
    Co-authored-by: Amidou <amidou.diallo@perfectogroupe.fr>

---
+++ constants.js
@@ -0,0 +1,6 @@
+// Constantes globales pour le backend
+const DEFAULT_IMAGE = "https://spotify-ynov.s3.eu-west-3.amazonaws.com/defaults/default-album.png";
+
+module.exports = {
+    DEFAULT_IMAGE
+}; 
